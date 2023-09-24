a17fa504eeef794b7010bb3eb4a1933309d5295f
TakumiFujita <46127387+TakumiFujita@users.noreply.github.com>
2023-09-24 12:11:46 +0900

    一次作成完了

---
+++ app/Http/Requests/TaskRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:30',
+            'content' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'タスクは必須です',
+            'name.max' => 'タスクは30文字以内で入力してください',
+            'content.required' => 'タスク内容は必須です',
+        ];
+    }
+}
