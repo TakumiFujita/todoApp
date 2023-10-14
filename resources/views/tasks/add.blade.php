4bf1eaa1f083d3f52e396aa28e7f64f69d265acb
TakumiFujita <46127387+TakumiFujita@users.noreply.github.com>
2023-10-14 17:49:34 +0900

    Applying tailwind css

--- resources/views/tasks/add.blade.php
+++ resources/views/tasks/add.blade.php
@@ -1,60 +1,70 @@
-<style>
-    h1 {
-        text-align: center;
-        padding: 30px;
-    }
+@extends('layout')
 
-    .form {
-        width: 80%;
-        margin: 0 auto;
-        text-align: center;
-    }
+@section('title', 'add')
 
-    .form-group {
-        padding-bottom: 50px;
-    }
+@section('content')
+    <style>
+        h1 {
+            text-align: center;
+            padding: 30px;
+        }
 
-    span {
-        color: red;
-    }
+        .form {
+            width: 80%;
+            margin: 0 auto;
+            text-align: center;
+        }
 
-    input {
-        width: 60%;
-        height: 30px;
-    }
+        .form-group {
+            padding-bottom: 50px;
+        }
 
-    textarea {
-        width: 60%;
-    }
+        span {
+            color: red;
+        }
 
-    .error {
-        text-align: center;
-    }
+        input {
+            width: 60%;
+            height: 30px;
+            border: 1px solid #999999;
+        }
 
-    .error__message {
-        color: red;
-    }
-</style>
-<h1>タスク追加</h1>
-<div class="error">
-    @foreach ($errors->all() as $error)
-        <p class="error__message">{{ $error }}</p>
-    @endforeach
-</div>
-<form action="{{ route('tasks.store') }}" method="POST" class="form">
-    @csrf
-    <div class="form-group">
-        <label for="name">タスク<span>(必須)</span></label><br>
-        <input type="text" name="name" maxlength="30" placeholder="タスクは30文字で書きましょう。" value="{{ old('name') }}">
+        textarea {
+            width: 60%;
+            border: 1px solid #999999;
+        }
+
+        .error {
+            text-align: center;
+        }
+
+        .error__message {
+            color: red;
+        }
+    </style>
+    <h1 class="text-4xl font-normal">タスク追加</h1>
+    <div class="error">
+        @foreach ($errors->all() as $error)
+            <p class="error__message">{{ $error }}</p>
+        @endforeach
     </div>
-    <div class="form-group">
-        <label for="content">タスク内容<span>(必須)</span></label><br>
-        <textarea rows="5" name="content" placeholder="タスク内容を具体的に書きましょう">{{ old('content') }}</textarea>
-    </div>
-    <div class="form-group">
-        <label for="deadline">期限</label><br>
-        <input type="date" name="deadline" min={{ $nowDate }} value="{{ old('deadline') }}">
-    </div>
-    <a href="/">戻る</a>
-    <button type="submit">追加する</button>
-</form>
+    <form action="{{ route('tasks.store') }}" method="POST" class="form">
+        @csrf
+        <div class="form-group">
+            <label for="name">タスク<span>(必須)</span></label><br>
+            <input type="text" name="name" maxlength="30" placeholder="タスクは30文字で書きましょう。" value="{{ old('name') }}">
+        </div>
+        <div class="form-group">
+            <label for="content">タスク内容<span>(必須)</span></label><br>
+            <textarea rows="5" name="content" placeholder="タスク内容を具体的に書きましょう">{{ old('content') }}</textarea>
+        </div>
+        <div class="form-group">
+            <label for="deadline">期限</label><br>
+            <input type="date" name="deadline" min={{ $nowDate }} value="{{ old('deadline') }}">
+        </div>
+        <a href="/"
+            class="bg-blue-500 hover:bg-blue-600 text-white rounded px-4 py-2 mr-4 inline-flex items-center">戻る</a>
+        <button type="submit"
+            class="bg-blue-500 hover:bg-blue-600 text-white rounded px-4 py-2 mr-4 inline-flex items-center">追加する</button>
+    </form>
+@endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 
-@section('title', 'add')
+@section('title', 'edit')
 
 @section('content')
     <style>
